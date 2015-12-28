6d9c9ba5f3849d2935584ba0f48fc297da81567d
yuyang <yuyang0805@gmail.com>
2015-12-28 22:32:00 +0800

    增加模版文件index.html

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "This is Leo"
+@app.route('/<name>', methods=['POST', 'GET'])
+def index(name=None):
+	return render_template('index.html', name=name)
 
 if __name__ == "__main__":
 	app.run()
