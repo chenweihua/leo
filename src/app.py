762d864040d04f8b7ee9e8308a4f181537fc4407
yuyang <yuyang0805@gmail.com>
2015-12-29 10:00:24 +0800

    增加一个404页面

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, errorhandler
 
 app = Flask(__name__)
 
 @app.route('/<name>', methods=['POST', 'GET'])
 def index(name=None):
+    if name == None:
+        name = ''
 	return render_template('index.html', name=name)
+
+@app.route('/admin', methods=['POST', 'GET'])
+def admin(name=None):
+    resp = make_response(render_template('admin.html'))
+    resp.headers['X-Powered-By'] = 'Leo'
+    resp.set_cookie('SESSION_ID', 'DU998IU')
+    return resp
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = make_response(render_template('404.html'))
+    return resp
 
 if __name__ == "__main__":
 	app.run()
